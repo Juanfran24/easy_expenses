82c1228d3ac58e11180c227881fa00b6d4278557
Arme94 <arnoldmenss@gmail.com>
2025-04-12 18:56:31 -0500

    ajuste en screen login

--- src/screens/Authentication/Login/index.tsx
+++ src/screens/Authentication/Login/index.tsx
@@ -1,53 +1,69 @@
-import { AppButton } from "@/src/components/Button";
 import { FlexBox } from "@/src/components/FlexBox";
-import { FlexBetween } from "@/src/components/FlexBox/FlexBetween";
 import { AppTextInput } from "@/src/components/Inputs/AppTextInput";
 import Typography from "@/src/components/Typography";
 import colors from "@/src/constants/colors";
 import { useAuth } from "@/src/context/AuthContext/useAuth";
-import { auth } from "@/src/database";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { AppButton } from "@/src/components/Button";
+import { useNavigation } from "@react-navigation/native";
 import React, { useState } from "react";
 
 const Login = () => {
-  const [user, setUser] = useState<string>("");
-  const [password, setpassword] = useState<string>("");
+  // const [user, setUser] = useState<string>("");
+  // const [password, setpassword] = useState<string>("");
   const { handleLogin } = useAuth();
-
-  // const signUp = async () => {
-  //   try {
-  //     const res = await createUserWithEmailAndPassword(auth, user, password);
-  //     console.log(res);
-  //   } catch (error) {
-  //     console.error("Error al registrar usuario:", error);
-  //   }
-  // };
-
   return (
-    <FlexBox style={{ backgroundColor: colors.backgrounds.base, flex: 1 }}>
-      <Typography.H2>Iniciar sesión</Typography.H2>
-      <AppTextInput
-        label="Correo electrónico"
-        placeholder="Username"
-        value={user}
-        onChangeText={setUser}
-      />
-      <AppTextInput
-        label="Contraseña"
-        placeholder="Password"
-        value={password}
-        onChangeText={setpassword}
-        type="password"
-      />
-      <AppButton
-        title="Sign In"
-        onPress={() => handleLogin()}
-        variant="outlined"
-      />
-      <FlexBetween>
-        <Typography.H1>Sign Up</Typography.H1>
-        <Typography.H1>Sign In</Typography.H1>
-      </FlexBetween>
+    <FlexBox
+      style={{
+        backgroundColor: colors.backgrounds.base,
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: "center",
+      }}
+    >
+      <FlexBox style={{ gap: 40 }}>
+        <Typography.H2 styles={{ textAlign: "center" }}>
+          Iniciar sesión
+        </Typography.H2>
+        <FlexBox style={{ width: "100%", gap: 20 }}>
+          <AppTextInput
+            label="Correo electrónico"
+            placeholder="ejemplo@correo.com"
+            type="email"
+          />
+          <AppTextInput
+            label="Contraseña"
+            placeholder="Ingresar"
+            type="password"
+          />
+        </FlexBox>
+        <Typography.P3.Underline
+          styles={{ textAlign: "right", color: colors.primary.main }}
+        >
+          ¿Has olvidado la contraseña?
+        </Typography.P3.Underline>
+        <FlexBox style={{ width: "100%", gap: 16 }}>
+          <AppButton
+            title="Iniciar sesión"
+            variant="contained"
+            onPress={handleLogin}
+          />
+          <AppButton
+            title="Ingresar con Google"
+            variant="outlined"
+            onPress={() => console.log("Ingresar con Google presionado")}
+          />
+          <AppButton
+            title="Ingresar con Apple"
+            variant="outlined"
+            onPress={() => console.log("Ingresar con Apple presionado")}
+          />
+          <AppButton
+            title="Registrarse"
+            variant="outlined"
+            onPress={() => navigation.navigate("Register")}
+          />
+        </FlexBox>
+      </FlexBox>
     </FlexBox>
   );
 };
