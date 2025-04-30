7182df0fd5c34d9ca679fac50183ef054fb2a5aa
Arme94 <arnoldmenss@gmail.com>
2025-04-29 22:04:21 -0500

    screen recuperar contraseña y componente alertmodal

--- src/navigation/auth/index.tsx
+++ src/navigation/auth/index.tsx
@@ -2,11 +2,13 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Landing from "../../screens/Authentication/Landing";
 import Register from "../../screens/Authentication/Register";
 import Login from "../../screens/Authentication/Login";
+import ResetPassword from "../../screens/Authentication/ResetPassword";
 
 export type AuthStackParamList = {
   Landing: undefined;
   Register: undefined;
   Login: undefined;
+  ResetPassword: undefined;
 };
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
@@ -27,6 +29,11 @@ function AuthStack() {
       <Stack.Screen
         name="Login"
         component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="ResetPassword"
+        component={ResetPassword}
         options={{ headerShown: false }}
       />
     </Stack.Navigator>
