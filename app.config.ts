31f6ba12da857876bd1a89a965a2806023222969
Juanfran24 <xxxestebanfrancoxxx@gmail.com>
2025-05-05 20:12:22 -0500

    nuevos ajustes necesarios para variables de entorno

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,8 @@
 export default {
   expo: {
+    owner: "easy_expenses",
     name: "easy_expenses",
-    slug: "easy_expenses",
+    slug: "easy-expenses-app",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
@@ -36,6 +37,11 @@ export default {
     ],
     experiments: {
       typedRoutes: true,
+    },
+    extra: {
+      eas: {
+        projectId: "a3893fe2-5a78-4524-84c7-59af0415c425",
+      },
     },
   },
 };
